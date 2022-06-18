7568d414462d82294163e8c1164bb6e5285a80ee
Ryokoboy <arystan.ye@gmail.com>
2022-06-19 00:29:02 +0600

    6/18/22 commit

--- hope_app/views.py
+++ hope_app/views.py
@@ -1,5 +1,5 @@
 import os
-import urllib
+import urllib.request
 from xml.dom import minidom
 
 import apiclient
@@ -15,27 +15,22 @@ from hope_app.models import Order
 
 
 class IndexView(TemplateView):
-    # Main page
-    template_name = "hope_app/main.html"
+    template_name = "hope_app/main.html"        # main.html created
 
 
 def api_connect():
-    # Get data from credential.json
-    # Path .\static\credential.json
     current_direction = os.path.dirname(os.path.abspath(__file__))
-    credential_json = '{}\\static\\cred.json'.format(current_direction)
-    # Google sheets id
+    cred_json = '{}\\static\\cred.json'.format(current_direction)
     spreadsheet_id = '1gfou6Uw32IEcMM3ZeMVbOUUwn79BjDxlsC7DDcB3tqQ'
 
-    # We log in and get a service instance of API access
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        credential_json,
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(         # connected to Google API(Sheets, Drive)
+        cred_json,
         ['https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive'])
     httpauth = credentials.authorize(httplib2.Http())
-    service = apiclient.discovery.build('sheets', 'v4', http=httpauth)
+    service = apiclient.discovery.build('sheets', 'v4', http=httpauth)      # services from Sheets v.4
 
-    values = service.spreadsheets().values().get(
+    values = service.spreadsheets().values().get(       # got data from table
         spreadsheetId=spreadsheet_id,
         range='A1:D100',
         majorDimension='ROWS'
@@ -45,14 +40,10 @@ def api_connect():
 
 
 def get_currency():
-    # Link for cbr.ru to get all current currencies
-    web = urllib.request.urlopen('https://www.cbr.ru/scripts/XML_daily.asp')
-
-    # Parsing cbr.ru
-    dom = minidom.parseString(web.read())
+    url = urllib.request.urlopen('https://www.cbr.ru/scripts/XML_daily.asp')        # XML file from CBR
+    dom = minidom.parseString(url.read())       # parsing XML document and normalizing view of doc
     dom.normalize()
 
-    # Looking for Valute tag
     elements = dom.getElementsByTagName("Valute")
 
     for node in elements:
@@ -70,15 +61,12 @@ def get_currency():
 
 def get_data(request):
 
-    # Getting values from Google Sheets
-    sheet_value = api_connect()
+    sheet_value = api_connect()     # get data from sheet doc
+    USD = get_currency()        # new variable
 
-    # Getting current USDs currency
-    usd = get_currency()
-
-    # Saving data in database
     for values in sheet_value:
         i = 0
+
         for value in values:
             if value == '№':
                 break
@@ -89,37 +77,38 @@ def get_data(request):
                 order_num = value
                 i += 1
             elif i == 2:
-                price_usd = float(value)
+                price_USD = float(value)
                 i += 1
             elif i == 3:
                 date = value
-                price_rub = price_usd * usd
-                price_rub = float('{:.2f}'.format(price_rub))
+                price_RUB = price_USD * USD
+                price_RUB = float('{:.2f}'.format(price_RUB))
                 i += 1
             if i == 4:
+
                 try:
-                    # Updating data by id
                     order = Order.objects.get(id=id)
                     order.order_num = order_num
-                    order.price_USD = price_usd
+                    order.price_USD = price_USD
                     order.delivery_date = date
-                    order.price_RUB = price_rub
+                    order.price_RUB = price_RUB
                 except:
-                    # Creating new data
-                    order = Order(id=num,
+                    order = Order(id=num,               # uploading data
                                   order_num=order_num,
-                                  price_USD=price_usd,
+                                  price_USD=price_USD,
                                   delivery_date=date,
-                                  price_RUB=price_rub)
-                # Saving in database
-                order.save()
+                                  price_RUB=price_RUB)
 
-    # Getting data from database, order by id
-    result = Order.objects.order_by('id')
-    # Deleting data from database
-    if len(result) > len(sheet_value) - 1:
-        for i in range(len(sheet_value), len(result) + 1):
+                order.save()        # saved in database
+
+
+    orders = Order.objects.order_by('id')
+
+    if len(orders) > len(sheet_value) - 1:              # delete data
+
+        for i in range(len(sheet_value), len(orders) + 1):
             order = Order.objects.get(id=i)
             order.delete()
-    # Returning in Json format
-    return JsonResponse({"result": list(result.values())})
+
+
+    return JsonResponse({"response": list(orders.values())})    # response like json file
